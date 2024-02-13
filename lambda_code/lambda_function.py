6109f354826a9196893a1fb8a05f1a4fe0a98446
Raiti, Jason Z <jason.raiti@swri.org>
2024-02-13 11:31:14 -0600

    fix bat bot to use new openai api

--- lambda_code/lambda_function.py
+++ lambda_code/lambda_function.py
@@ -24,29 +24,30 @@ def lambda_handler(event, context):
     messages=[
         {
         "role": "system",
-        "content": "You are a Dall E prompt generator. You will be trying to create prompts to create art that features different species of fungi. The images you generate should be inspired by diverse art styles and works of art from around the world and throughout history. "
+        "content": "You are a Dall E prompt generator. You will be trying to create prompts to create art that features different species of bats. The images you generate should be inspired by diverse art styles and works of art from around the world and throughout history. "
         },
         {
         "role": "user",
-        "content": "I am an artist trying to promote the research and conservation of fungi via interesting and novel art forms. Please generate Dall-E prompts that utilize art styles from different periods of history and around the world and add fungi to them."
+        "content": "I am an artist trying to promote the conservation of bat populations via interesting and novel art forms. Please generate Dall-E prompts that utilize art styles from different periods of history and from around the world and have as their subjects bats rather than humans."
         },
         {
         "role": "assistant",
-        "content": "A traditional Japanese Ukiyo-e styled print depicting a serene landscape with giant mushrooms dropping spores instead of cherry blossom trees dropping leaves.\n An Aztec codex inspired illustration incorporating corn smut fungus, honoring their reverence for this delicacy, also known as huitlacoche \n.A Greek amphora illustration depicting the myth of Dionysus discovering wine through yeast fungi.\n An intricate, 17th-century Dutch Golden Age still life, including a variety of mushrooms and lichens among the typical fruits and flowers.\n An Art Nouveau masterpiece in the style of Gustav Klimt but with mycelium and hyphae connecting the figures instead of symbolic shapes."
+        "content": " A Victorian-style portrait of a Vampire Bat with intricate lace detailing. \n An Egyptian Hieroglyphic mural depicting the life cycle of an Egyptian Fruit Bat.\n Impressionist painting in Monet's style showcasing Mexican Free-tailed Bats flying at dusk over water lilies pond.\n Japanese Ukiyo-e woodblock print featuring Japanese Pipistrelle bats against Mount Fuji backdrop. \nThe Starry Night inspired post-impressionistic artwork showing Big Brown Bats swirling around cypress trees under starlit sky \nGuernica-style cubism art by Picasso, portraying Townsend’s big-eared bat amidst chaos and peace symbols \nAmerican Gothic parody where instead of farmers there are two Pallid bats holding farming tools standing before their cave home \nArt Nouveau poster design promoting conservation efforts for Indiana Myotis (Myotis sodalis) using vibrant colors and organic forms\n Pop Art representation à la Andy Warhol: Four panels each displaying different species - Little Red Flying Fox, Greater Bulldog bat, Lesser Short-nosed fruit bat & Kitti's hog-nosed bat \nSurrealist Salvador Dali-inspired melting clocks scene but replacing clocks with Spotted-winged fruit bats hanging from tree branches "
         },
         {
         "role": "user",
-        "content": "Thank you. Please provide one more."
+        "content": "Please provide one more."
         }
     ],
-        temperature=1.1,
-        max_tokens=50,
-        top_p=1,
-        frequency_penalty=1,
-        presence_penalty=1
+    temperature=1.15,
+    max_tokens=70,
+    top_p=1,
+    frequency_penalty=2,
+    presence_penalty=2
     )
     image_prompt = response.choices[0].message.content
     print('Image Prompt:', image_prompt)
+
 
     #Generate image based on image prompt
     response = openai_client.images.generate(
@@ -74,11 +75,11 @@ def lambda_handler(event, context):
         },
         {
         "role": "user",
-        "content": "Write an original short poem about fungi. Use evocative language. The poem should promote environmentalism OR be humorous OR be from the fungus. Make it no longer than 15 words."
+        "content": "Write an original short poem about bats. Use evocative language. The poem should promote environmentalism OR be humorous OR be from the perspective of a bat. Make it no longer than 15 words."
         },
         {
         "role": "user",
-        "content": ("For context, the following is a description of an image that will accompany your poem: " + image_prompt )
+        "content": ("for context, the following is a description of an image which will accompany your poem: " + image_prompt )
         }
     ],
     temperature=1,
@@ -92,14 +93,14 @@ def lambda_handler(event, context):
     print('Caption:', poem)
 
     #Declare Hashtags
-    hashtags_list = [ "#mushroom", "#fungi", "#mushroomart", "#mycologysociety", "#mycology", "#psychedelic", "#psychedelicart", "#ai", "#chatgpt", "#dalle", "mushroomartworks", "#fungilove" ]
+    hashtags_list = [ "#savethebats", "#batsarecool", "#batsofinstagram", "#Bats", "#BatLove", "#fruitbat", "#aiart", "#ai", "#chatgpt", "#dalle", "#bat" ]
     hashtags_string = " ".join(str(x) for x in hashtags_list)
 
     #Make compound caption:
     caption = """{one}
 
-    Support the research and conservation of fungi:
-    https://www.centraltexasmycology.org/
+    Support the research and conservation of bats:
+    https://www.batcon.org/
 
     {three}
     """.format(one=poem, three= hashtags_string)
@@ -114,7 +115,6 @@ def lambda_handler(event, context):
     os.chdir("/tmp")
     image.save("ai_mush_pic.jpg")
     CustomTools.post_image_and_caption( "./ai_mush_pic.jpg" , caption, instagrapi_client )
-
 
     #-----------------------------------
     return "success"  # Echo back the first key value
